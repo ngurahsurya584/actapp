3275e4af724005134216ccf34878a469b9909ffd
ngurahsurya584 <ngurahgusti584@gmail.com>
2024-05-06 16:04:37 +0700

    Initial Commit

---
+++ actapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  actapp
+//
+//  Created by I Gusti Ngurah Surya Ardika Dinataputra on 06/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ actapp/actappApp.swift
@@ -0,0 +1,17 @@
+//
+//  actappApp.swift
+//  actapp
+//
+//  Created by I Gusti Ngurah Surya Ardika Dinataputra on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct actappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
