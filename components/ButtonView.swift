22a0799252106231f24e821335601146c000583a
ngurahsurya584 <ngurahgusti584@gmail.com>
2024-05-07 17:34:15 +0700

    bikin file button view

---
+++ components/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  actapp
+//
+//  Created by I Gusti Ngurah Surya Ardika Dinataputra on 07/05/24.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
