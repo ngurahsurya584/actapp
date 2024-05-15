50b11f6c2e2a6557077e39503dc5b40597035aab
Channy <channylim24@gmail.com>
2024-05-15 16:38:10 +0700

    Add Being Kind Affirmation Copy

---
+++ nightjournaling/BeingKind - AffirmationCopy.swift
@@ -0,0 +1,18 @@
+//
+//  BeingKind - AffirmationCopy.swift
+//  actapp
+//
+//  Created by Channy Lim on 15/05/24.
+//
+
+import SwiftUI
+
+struct BeingKind___AffirmationCopy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BeingKind___AffirmationCopy()
+}
